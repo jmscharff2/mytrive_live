ea38f4e4058047134822bdeb796eb7f24f347ba7
Jonathan Scharff II <jscharff2@gmail.com>
2012-12-05 15:00:07 -0500

    added meny files for javascript side menu and updated it to the files.php site-  will add to rest of site later.

--- files.php
+++ files.php
@@ -3,9 +3,15 @@
 ?>
 
 <HTML>
+	<div class="meny">
+		<H2> Files go here</H2>
+	</div>
+
+	<div class="contents">
 <?php
 	//session_start();
-
+	
+	
 
 	if(isset($_SESSION['username']) && $_SESSION['username'] != '')
 	{
@@ -17,9 +23,52 @@
 		echo '<meta http-equiv="Refresh" content="1; URL=http://www.mytrive.com">';
 	}
 
-?>
+?>	
+		
+	<script src="js/meny.min.js"></script>
+		
+		<script>
+		
+// Create an instance of Meny
+			var meny = Meny.create({
+				// The element that will be animated in from off screen
+				menuElement: document.querySelector( '.meny' ),
 
-	<BODY>
+				// The contents that gets pushed aside while Meny is active
+				contentsElement: document.querySelector( '.contents' ),
+
+				// [optional] The alignment of the menu (top/right/bottom/left)
+				position: 'left',
+
+				// [optional] The height of the menu (when using top/bottom position)
+				height: 200,
+
+				// [optional] The width of the menu (when using left/right position)
+				width: 260,
+
+				// [optional] Distance from mouse (in pixels) when menu should open
+				threshold: 40
+			});
+			
+			
+			// API Methods:
+			// meny.open();
+			// meny.close();
+			// meny.isOpen();
+			
+			// Events:
+			// meny.addEventListener( 'open', function(){ console.log( 'open' ); } );
+			// meny.addEventListener( 'close', function(){ console.log( 'close' ); } );
+
+			// Embed an iframe if a URL is passed in
+			if( Meny.getQuery().u && Meny.getQuery().u.match( /^http/gi ) ) {
+				var contents = document.querySelector( '.contents' );
+				contents.style.padding = '0px';
+				contents.innerHTML = '<div class="cover"></div><iframe src="'+ Meny.getQuery().u +'" style="width: 100%; height: 100%; border: 0; position: absolute;"></iframe>';
+			}
+</script>
+
+	
 		  <div class="scroll"></div>
 		  <?php include 'includes/sidenav.html';?>
        		
@@ -219,7 +268,7 @@ if($result)
 	</div>
 	</div>
 	<?php include 'includes/footer.html'?>
-
+	</div>
 	</body>
 	
 </HTML>
