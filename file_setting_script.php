a664d2ebfd727f8d3e5fd46fabf1ad82cd8d0942
Jonathan Scharff II <jscharff2@gmail.com>
2012-12-06 01:43:21 -0500

    test script added for renames

--- file_setting_script.php
+++ file_setting_script.php
@@ -65,7 +65,9 @@ require_once('config.php');
 		
 		
 	$old_file_name = $file_path."/".$origional_file_name;
+	echo $old_file_name."<br>";
 	$new_file_name = $file_path."/".$file_name;
+	echo $new_file_name;
 	$qry = "UPDATE files SET file_name = '$file_name', want_to_share = '$sharing' WHERE file_id = '$file_id'";
 
 	$result = @mysql_query($qry);
@@ -73,13 +75,13 @@ require_once('config.php');
 	if($result)
 	{
 		//echo "Good";
-		rename('$old_file_name', '$new_file_name');
-		header("location: files.php");
+		rename($old_file_name, $new_file_name);
+		//header("location: files.php");
 	}
 	else
 	{
 		//echo "Bad";
-		header("location: files_error.php");
+		//header("location: files_error.php");
 	}
 
 ?>
