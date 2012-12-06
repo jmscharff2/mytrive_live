ee69eec6ed1cfd91685a6626850ed85f6404738a
Jonathan Scharff II <jscharff2@gmail.com>
2012-12-06 01:13:54 -0500

    missing '

--- file_settings.php
+++ file_settings.php
@@ -173,7 +173,7 @@
 						}
 						echo "</td>";
 						echo "</tr>";
-						echo "<input type='text name='file_id' value=".$member['file_id'].">";
+						echo "<input type='text' name='file_id' value=".$member['file_id'].">";
 						
 			
 						//echo "/".$member['location']."/".$member['file_name'];
