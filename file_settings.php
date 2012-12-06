3b5b665e5ec6aea5fb94cbf94f316692e2c2eacc
Jonathan Scharff II <jscharff2@gmail.com>
2012-12-06 01:07:24 -0500

    fixed closing >

--- file_settings.php
+++ file_settings.php
@@ -173,7 +173,7 @@
 						}
 						echo "</td>";
 						echo "</tr>";
-						echo "<input type='text name='file_id' value=".$member['file_id']."'hidden='true'>";
+						echo "<input type='text name='file_id' value=".$member['file_id']."'hidden='false'/>";
 						
 			
 						//echo "/".$member['location']."/".$member['file_name'];
