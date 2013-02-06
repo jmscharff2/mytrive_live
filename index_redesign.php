6a351d817573c9ac7595f7bb4fd0b5ea72f1771a
Jonathan Scharff II <jscharff2@gmail.com>
2013-02-05 21:30:41 -0500

    redesign files for the new layout for the landing page -- no database code is included yet

---
+++ index_redesign.php
@@ -0,0 +1,52 @@
+<?php
+
+	session_start();
+	
+?>
+
+<HTML>
+
+<HEAD>
+
+<TITLE>mytrive redesign</TITLE>
+
+<link rel="stylesheet" type="text/css" href="css/redesign.css" />
+
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+
+<link rel="icon" 
+      type="image/png" 
+      href="images/favicon.ico">
+
+
+</HEAD>
+<BODY>
+<nav>
+<a href="#"><img src = "images/trive.png" style="margin-top: -10px;"></a>
+<ul>
+<li><a href="#">Profile</a></li>
+<li><a href="#">Files</a></li>
+<li><a href="#">Friends</a></li>
+<form>
+<li><input type="text" name="username" placeholder="username"/></li>
+<li><input type="password" name="username" placeholder="password"/></li>
+</form>
+</ul>
+</nav>
+
+<section>
+<form>
+<label>Username</label>
+<input type="text" name="username" placeholder="username"/>
+<label>Email</label>
+<input type="text" name="email" placeholder="email"/>
+<label>Password</label>
+<input type="password" name="password" placeholder="password"/>
+<label>Confirm Password</label>
+<input type="password" name="cpassword" placeholder="confirm password"/>
+</form>
+</section>
+
+
+</BODY>
+</HTML>
