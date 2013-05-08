0b6b8b13baa210cb38bbf1b6e694e7692f41de66
Jonathan Scharff II <jscharff2@gmail.com>
2013-05-08 15:02:29 -0400

    forgot database connect code….dur

--- profile_redesign.php
+++ profile_redesign.php
@@ -2,6 +2,22 @@
 <?php
 	session_start();
 	$username = $_SESSION['username'];
+	
+	require_once('config.php');
+	require_once('includes/amazon.php');
+
+	$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+	if(!$link)
+	{
+	       	 die('Failed to connect to server: '. mysql_error());
+	}
+
+	$db = mysql_select_db(DB_DATABASE);
+	if(!$db)
+	{	
+		die("Unable to select database");
+	}
+
 ?>
 
 <HTML lang="en-us">
