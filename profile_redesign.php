372407372ae38c6a253776bacf4144c61a3500b9
Jonathan Scharff II <jscharff2@gmail.com>
2013-05-09 01:41:01 -0400

    no message

--- profile_redesign.php
+++ profile_redesign.php
@@ -314,7 +314,7 @@ Drag and Drop your files here!
 				{
 					$member = mysql_fetch_assoc($result);
 					
-					$file2 = $member['owner_id']."/".$member['file_name'];
+					$file2 = $member['profile_picture'];
 					$bucket2 = "mytrive_files";
 					$download_file_string2 = gs_prepareS3URL($file2, $bucket2);
 					
