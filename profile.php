a895631ef3c0fd73df33fb32e4ce7a84c6adb051
Jonathan Scharff II <jscharff2@gmail.com>
2013-05-09 15:40:07 -0400

    no message

--- profile.php
+++ profile.php
@@ -171,95 +171,6 @@ $(document).ready(function(){
 
 
 
-	function() {
-
-  $("#uploader").plupload({
-
-      // General settings
-
-      runtimes : 'gears,flash,silverlight,browserplus,html5',
-
-      url : 'upload_file.php',
-
-      max_file_size : '10mb',
-
-      chunk_size : '1mb',
-
-      unique_names : true,
-
-
-
-      // Resize images on clientside if we can
-
-      resize : {width : 320, height : 240, quality : 90},
-
-
-
-      // Specify what files to browse for
-
-      filters : [
-
-          {title : "Image files", extensions : "jpg,gif,png"},
-
-          {title : "Zip files", extensions : "zip"}
-
-      ],
-
-
-
-      // Flash settings
-
-      flash_swf_url : '/plupload/js/plupload.flash.swf',
-
-
-
-      // Silverlight settings
-
-      silverlight_xap_url : '/plupload/js/plupload.silverlight.xap'
-
-  });
-
-
-
-  // Client side form validation
-
-  $('form').submit(function(e) {
-
-      var uploader = $('#uploader').plupload('getUploader');
-
-
-
-      // Files in queue upload them first
-
-      if (uploader.files.length > 0) {
-
-          // When all files are uploaded submit form
-
-          uploader.bind('StateChanged', function() {
-
-              if (uploader.files.length === (uploader.total.uploaded + uploader.total.failed)) {
-
-                  $('form')[0].submit();
-
-              }
-
-          });
-
-               
-
-          uploader.start();
-
-      } else
-
-          alert('You must at least upload one file.');
-
-
-
-      return false;
-
-  });
-
-});
 
 
 
@@ -267,8 +178,7 @@ $(document).ready(function(){
 
 
 
-
-		/*		
+			
 		
 		
 		$(function() {
@@ -292,7 +202,7 @@ $(document).ready(function(){
 					{title : "Book Files", extensions : "mobi"}	
 			     ]	
 			     });
-			 });*/
+			 });
 			 
 			 function showDiv(section) {
 			if(section == "friend_content")
