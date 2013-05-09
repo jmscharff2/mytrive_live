d63f17b6227a0c186d290cf57ba05f9e3854f7f3
Jonathan Scharff II <jscharff2@gmail.com>
2013-05-09 15:49:02 -0400

    no message

--- profile.php
+++ profile.php
@@ -173,103 +173,12 @@ $(document).ready(function(){
 
 
 
-$(function() {
-
-  $("#uploader").plupload({
-
-      // General settings
-
-      runtimes : 'gears,flash,silverlight,browserplus,html5',
-
-      url : 'upload.php',
-
-      max_file_size : '10mb',
-
-      chunk_size : '1mb',
-
-      unique_names : true,
-
-
-
-      // Resize images on clientside if we can
-
-      resize : {width : 320, height : 240, quality : 90},
-
-
-
-      // Specify what files to browse for
-
-      filters : [
-
-          {title : "Image files", extensions : "jpg,gif,png"},
-
-          {title : "Zip files", extensions : "zip"}
-
-      ],
-
-
-
-      // Flash settings
-
-      flash_swf_url : '/js/plupload.flash.swf',
-
-
-
-      // Silverlight settings
-
-      silverlight_xap_url : '/js/plupload.silverlight.xap'
-
-  });
-
-
-
-  // Client side form validation
-
-  $('form').submit(function(e) {
-
-      var uploader = $('#uploader').plupload('getUploader');
-
-
-
-      // Files in queue upload them first
-
-      if (uploader.files.length > 0) {
-
-          // When all files are uploaded submit form
-
-          uploader.bind('StateChanged', function() {
-
-              if (uploader.files.length === (uploader.total.uploaded + uploader.total.failed)) {
-
-                  $('form')[0].submit();
-
-              }
-
-          });
-
-               
-
-          uploader.start();
-
-      } else
-
-          alert('You must at least upload one file.');
-
-
-
-      return false;
-
-  });
-
-});
 
 
 
 
 
-
-
-		/*		
+		
 		
 		
 		$(function() {
@@ -293,7 +202,7 @@ $(function() {
 					{title : "Book Files", extensions : "mobi"}	
 			     ]	
 			     });
-			 });*/
+			 });
 			 
 			 function showDiv(section) {
 			if(section == "friend_content")
@@ -918,11 +827,8 @@ $(function() {
 	    <a id="uploadfiles" href="javascript:;">[Upload files]</a>
 	    </div>
 		</center>-->
-		<form>
-		<div id="uploader">
-        <p>You browser doesn't have Flash, Silverlight, Gears, BrowserPlus or HTML5 support.</p>
-        </div>
-		</form>
+		<div id="html5_uploader">You browser doesn't support native upload. Try Firefox 3 or Safari 4.</div>
+
 		</div>
 <!--
 		<script>
