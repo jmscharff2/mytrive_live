8778f5b8226e27a606c2356f4e154401f4234483
Jonathan Scharff II <jscharff2@gmail.com>
2013-05-11 18:47:16 -0400

    no message

--- settings.php
+++ settings.php
@@ -149,7 +149,6 @@ if($result)
 	    <?php
 	    
 	     $cursor = $coll->find(array("username" => $username));
-	     $cursor->sort(array('date' => 1));
 	     echo "Your last 10 pages viewed";
 	     $z = 0;
 	     foreach ($cursor as $doc) {
